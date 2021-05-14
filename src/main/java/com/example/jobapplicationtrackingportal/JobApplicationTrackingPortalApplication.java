59a6a3999d43d304e76d092c9cebb319c853c7ad
Shubham Rane <official.shubhamrane@gmail.com>
2021-05-14 18:53:11 -0400

    Initial Commit

---
+++ src/main/java/com/example/jobapplicationtrackingportal/JobApplicationTrackingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobapplicationtrackingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationTrackingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobapplicationtrackingportal/JobApplicationTrackingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobapplicationtrackingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
